54a5a0d81b1d7cdceef8d2d9a94580ea02640348
Vitaliy <vitaliy-efimov111@mail.ru>
2024-02-29 00:19:01 +0500

    Updated test logic

--- bot/src/test/java/edu/java/bot/command/CommandTest.java
+++ bot/src/test/java/edu/java/bot/command/CommandTest.java
@@ -3,23 +3,24 @@ package edu.java.bot.command;
 import com.pengrad.telegrambot.model.Chat;
 import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
-import edu.java.bot.repository.UserChatRepository;
+import edu.java.bot.service.client.ScrapperClient;
+import edu.java.bot.service.client.ScrapperClientBuilder;
 import org.junit.jupiter.api.BeforeEach;
 import org.mockito.Mockito;
 
 public abstract class CommandTest {
     protected Update update;
     protected Message message;
-    protected UserChatRepository userChatRepository;
     protected Long chatId;
+    protected ScrapperClient client;
 
     @BeforeEach
     public void setUp() {
         update = Mockito.spy(new Update());
         message = Mockito.spy(new Message());
         Chat chat = Mockito.spy(new Chat());
-        userChatRepository = Mockito.spy(new UserChatRepository());
         chatId = 123L;
+        client = Mockito.spy(new ScrapperClientBuilder().build());
 
         Mockito.doReturn(message).when(update).message();
         Mockito.doReturn(chat).when(message).chat();
