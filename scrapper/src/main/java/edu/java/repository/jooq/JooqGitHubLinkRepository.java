dea8976517e11ef84f498ed8e5baf511f60a49a9
Vitaliy <vitaliy-efimov111@mail.ru>
2024-03-16 21:55:10 +0500

    Added jooq services and repositories implementation

---
+++ scrapper/src/main/java/edu/java/repository/jooq/JooqGitHubLinkRepository.java
@@ -0,0 +1,44 @@
+package edu.java.repository.jooq;
+
+import edu.java.entity.GitHubLink;
+import edu.java.entity.Link;
+import edu.java.repository.SpecificLinkRepository;
+import lombok.RequiredArgsConstructor;
+import org.jooq.DSLContext;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import static ru.tinkoff.edu.java.scrapper.domain.jooq.tables.GithubLink.GITHUB_LINK;
+
+@Repository
+@RequiredArgsConstructor
+public class JooqGitHubLinkRepository implements SpecificLinkRepository<GitHubLink> {
+    private final DSLContext dslContext;
+
+    @Override
+    @Transactional
+    public GitHubLink getLink(Link link) {
+        return dslContext.select(GITHUB_LINK.fields())
+            .from(GITHUB_LINK)
+            .where(GITHUB_LINK.ID.eq(link.getId()))
+            .fetchOneInto(GitHubLink.class);
+    }
+
+    @Override
+    @Transactional
+    public GitHubLink addLink(GitHubLink link) {
+        return dslContext.insertInto(GITHUB_LINK, GITHUB_LINK.ID, GITHUB_LINK.DEFAULT_BRANCH, GITHUB_LINK.FORKS_COUNT)
+            .values(link.getId(), link.getDefaultBranch(), link.getForksCount())
+            .returning(GITHUB_LINK.fields())
+            .fetchOneInto(GitHubLink.class);
+    }
+
+    @Override
+    @Transactional
+    public void updateLink(GitHubLink link) {
+        dslContext.update(GITHUB_LINK)
+            .set(GITHUB_LINK.DEFAULT_BRANCH, link.getDefaultBranch())
+            .set(GITHUB_LINK.FORKS_COUNT, link.getForksCount())
+            .where(GITHUB_LINK.ID.eq(link.getId()))
+            .execute();
+    }
+}
